4f4041fc74ad3885511d700596f0dcb0fd7f176a
xufeng <xufeng@shopee.com>
2022-04-04 14:00:44 +0800

    feat: add git secret at url

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { MongooseModule } from '@nestjs/mongoose'
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [MongooseModule
-    .forRoot('mongodb://user:pass@mongodb:27017/',{useNewUrlParser:true}), UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL,{useNewUrlParser:true}), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
