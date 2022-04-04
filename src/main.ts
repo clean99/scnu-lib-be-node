e5c86b11281f4d2d9b7d4ca496a666c26ec7b3a7
xufeng <xufeng@shopee.com>
2022-04-04 15:33:07 +0800

    fix: fix path prefix due to nginx

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import {SwaggerModule,DocumentBuilder} from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.MONGODB_URL)
+  app.setGlobalPrefix('apis');
   const config = new DocumentBuilder()
     .setTitle('SCNU-LIB-BE')
     .setDescription('The scnu-lib-be nodejs implement API document.')
@@ -11,7 +11,7 @@ async function bootstrap() {
     .addTag('user')
     .build();
   const document = SwaggerModule.createDocument(app,config);
-  SwaggerModule.setup('api',app,document);
+  SwaggerModule.setup('swagger',app,document);
   await app.listen(3000);
 }
 bootstrap();
