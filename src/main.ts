7c6815f9f27ed84e9b60b77c77a700550c809887
xufeng <xufeng@shopee.com>
2022-04-09 22:05:48 +0800

    fix: delete useless imp and change port

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('apis/swagger', app, document);
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
